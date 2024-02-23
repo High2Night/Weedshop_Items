23f9ec6b3f8d5dcf5d63cbb6396479ab2179a21c
High2Night <160995381+High2Night@users.noreply.github.com>
2024-02-23 05:47:18 +0100

    Add files via upload

---
+++ ready/KH-weedshop-items.lua
@@ -0,0 +1,123 @@
+
+-- Bags
+
+["KH_cakeMix"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["KH_cerealMilk"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["KH_georgiaPie"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["KH_jefe"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["KH_snowMan"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["KH_whiteRuntz"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["KH_whitecherryGelato"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["KH_blueberryCruffin"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+
+-- Joints
+
+["joint_blueberryCruffin"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["joint_cakeMix"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["joint_cerealMilk"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["joint_georgiaPie"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["joint_jefe"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["joint_snowMan"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["joint_whitecherryGelato"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["joint_whiteRuntz"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+
+-- Papes
+
+["backwoods_grape"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["backwoods_honey"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
+["backwoods_russian_cream"] = {
+    label = "Ak74",
+    weight = 1,
+    stack = true,
+    close = true,
+},
